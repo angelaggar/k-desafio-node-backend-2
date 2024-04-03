881f13c26d7cb775f89ef753ee1121511fd8b57a
DEVFBA <angenrique.gutierrez@gmail.com>
2024-04-02 21:33:37 -0600

    Send PORT to env var

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-require('dotenv').config() 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = 3002
-const mongoDB = require('./src/db/dbM')
-const user = require('./src/routes/user')
-const post = require('./src/routes/posts')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3002;
+const mongoDB = require("./src/db/dbM");
+const user = require("./src/routes/user");
+const post = require("./src/routes/posts");
 
+app.use(express.json());
 
-app.use(express.json())
+app.use(cors());
 
-app.use(cors())
+app.use("/user", user);
+app.use("/post", post);
 
-
-app.use('/user', user)
-app.use('/post', post)
-
-app.get('/', (req, res) => {
-  res.status(200).send('Hey there!')
-})
+app.get("/", (req, res) => {
+  res.status(200).send("Hey there!");
+});
 
 mongoDB.connect
   .then((message) => {
-    console.log(message)
+    console.log(message);
     app.listen(port, () => {
-      console.log(`Server listening on ${port} port.`)
-    })
+      console.log(`Server listening on ${process.env.PORT} port.`);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
